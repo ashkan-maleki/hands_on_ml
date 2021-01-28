aff1352054e6734991a0d1fe0976d2c63575d07b
johnnywcage <johnnywcage@outlook.com>
2021-01-28 19:52:40 +0330

    handling null values in the dataset

--- ch02/__init__.py
+++ ch02/__init__.py
@@ -36,7 +36,7 @@ def run_ch02():
     for set_ in (strat_train_set, strat_test_set):
         set_.drop("income_cat", axis=1, inplace=True)
 
-    housing = strat_train_set.copy()
+    # housing = strat_train_set.copy()
     # housing.plot(kind="scatter", x="longitude", y="latitude")
     # housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
     # housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
@@ -46,6 +46,34 @@ def run_ch02():
     # corr_matrix = housing.corr()
     # print(corr_matrix["median_house_value"].sort_values(ascending=False))
 
-    attributes = ["median_house_value", "median_income", "total_bedrooms", "housing_median_age"]
-    scatter_matrix(housing[attributes], figsize=(12, 8))
-    plt.show()
+    # attributes = ["median_house_value", "median_income", "total_bedrooms", "housing_median_age"]
+    # scatter_matrix(housing[attributes], figsize=(12, 8))
+    # plt.show()
+
+    # housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+    # housing["bedrooms_per_room"] = housing["total_bedrooms"] / housing["total_rooms"]
+    # housing["population_per_household"] = housing["population"] / housing["households"]
+    #
+    # corr_matrix = housing.corr()
+    # corr_result = corr_matrix["median_house_value"].sort_values(ascending=False)
+    # print(corr_result)
+
+    housing = strat_train_set.drop("median_house_value", axis=1)
+    housing_labels = strat_train_set["median_house_value"].copy()
+
+    housing.dropna(subset=["total_bedrooms"])
+    housing.drop("total_bedrooms", axis=1)
+    median = housing["total_bedrooms"].median()
+    housing["total_bedrooms"].fillna(median, inplace=True)
+
+    from sklearn.impute import SimpleImputer
+    imputer = SimpleImputer(strategy="median")
+    housing_num = housing.drop("ocean_proximity", axis=1)
+    imputer.fit(housing_num)
+
+    print(imputer.statistics_)
+    print(housing_num.median().values)
+
+    X = imputer.transform(housing_num)
+    housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=housing_num.index)
+
