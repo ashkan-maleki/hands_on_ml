d56fadef00748be11ad5a70c8dcf9b20ee82aa33
johnnywcage <johnnywcage@outlook.com>
2021-02-19 15:35:41 +0330

    linear regression

---
+++ ch03/ex0403_titanic.py
@@ -0,0 +1,115 @@
+import os
+import pandas as pd
+
+import numpy as np
+
+np.random.seed(42)
+
+TITANIC_PATH = os.path.join("datasets", "titanic")
+
+
+def load_titanic_data(filename, titanic_path=TITANIC_PATH):
+    csv_path = os.path.join(titanic_path, filename)
+    return pd.read_csv(csv_path)
+
+
+train_data = load_titanic_data("train.csv")
+test_data = load_titanic_data("test.csv")
+
+train_data.head()
+
+train_data.info()
+train_data.describe()
+
+train_data["Survived"].value_counts()
+train_data["Pclass"].value_counts()
+train_data["Sex"].value_counts()
+train_data["Embarked"].value_counts()
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.attribute_names]
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
+num_pipeline = Pipeline([
+    ("select_numeric", DataFrameSelector(["Age", "SibSp", "Parch", "Fare"])),
+    ("imputer", SimpleImputer(strategy="median")),
+])
+
+num_pipeline.fit_transform(train_data)
+
+
+# Inspired from stackoverflow.com/questions/25239958
+class MostFrequentImputer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        self.most_frequent_ = pd.Series([X[c].value_counts().index[0] for c in X],
+                                        index=X.columns)
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.most_frequent_)
+
+
+from sklearn.preprocessing import OneHotEncoder
+
+cat_pipeline = Pipeline([
+        ("select_cat", DataFrameSelector(["Pclass", "Sex", "Embarked"])),
+        ("imputer", MostFrequentImputer()),
+        ("cat_encoder", OneHotEncoder(sparse=False)),
+    ])
+
+cat_pipeline.fit_transform(train_data)
+
+from sklearn.pipeline import FeatureUnion
+preprocess_pipeline = FeatureUnion(transformer_list=[
+        ("num_pipeline", num_pipeline),
+        ("cat_pipeline", cat_pipeline),
+    ])
+
+X_train = preprocess_pipeline.fit_transform(train_data)
+
+y_train = train_data["Survived"]
+
+from sklearn.svm import SVC
+
+svm_clf = SVC(gamma="auto")
+svm_clf.fit(X_train, y_train)
+
+X_test = preprocess_pipeline.transform(test_data)
+y_pred = svm_clf.predict(X_test)
+
+from sklearn.model_selection import cross_val_score
+
+svm_scores = cross_val_score(svm_clf, X_train, y_train, cv=10)
+svm_scores.mean()
+
+from sklearn.ensemble import RandomForestClassifier
+
+forest_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+forest_scores = cross_val_score(forest_clf, X_train, y_train, cv=10)
+forest_scores.mean()
+
+
+from matplotlib import pyplot as plt
+plt.figure(figsize=(8, 4))
+plt.plot([1]*10, svm_scores, ".")
+plt.plot([2]*10, forest_scores, ".")
+plt.boxplot([svm_scores, forest_scores], labels=("SVM","Random Forest"))
+plt.ylabel("Accuracy", fontsize=14)
+plt.show()
+
+
+train_data["AgeBucket"] = train_data["Age"] // 15 * 15
+train_data[["AgeBucket", "Survived"]].groupby(['AgeBucket']).mean()
--- ch04/__init__.py
+++ ch04/__init__.py
@@ -31,3 +31,41 @@ def run_ch04():
     print(theta_best_svd)
 
     print(np.linalg.pinv(X_b).dot(y))
+
+    eta = 0.1  # learning rate
+    n_iterations = 1000
+    m = 100
+
+    theta = np.random.randn(2, 1)  # random initialization
+
+    for iteration in range(n_iterations):
+        gradients = 2 / m * X_b.T.dot(X_b.dot(theta) - y)
+        theta = theta - eta * gradients
+
+    print(theta)
+
+    m = len(X_b)
+
+    n_epochs = 50
+    t0, t1 = 5, 50  # learning schedule hyperparameters
+
+    def learning_schedule(t):
+        return t0 / (t + t1)
+
+    theta = np.random.randn(2, 1)  # random initialization
+
+    for epoch in range(n_epochs):
+        for i in range(m):
+            random_index = np.random.randint(m)
+            xi = X_b[random_index:random_index + 1]
+            yi = y[random_index:random_index + 1]
+            gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+            eta = learning_schedule(epoch * m + i)
+            theta = theta - eta * gradients
+
+    from sklearn.linear_model import SGDRegressor
+
+    sgd_reg = SGDRegressor(max_iter=1000, tol=1e-3, penalty=None, eta0=0.1, random_state=42)
+    sgd_reg.fit(X, y.ravel())
+
+    print(sgd_reg.intercept_, sgd_reg.coef_)
