afbc2cda1594ec6a49328834acdfbf4571688b06
johnnywcage <johnnywcage@outlook.com>
2021-02-22 19:53:23 +0330

    latest changes

--- ch04/__init__.py
+++ ch04/__init__.py
@@ -69,3 +69,60 @@ def run_ch04():
     sgd_reg.fit(X, y.ravel())
 
     print(sgd_reg.intercept_, sgd_reg.coef_)
+
+    import numpy.random as rnd
+
+    m = 100
+    X = 6 * np.random.rand(m, 1) - 3
+    y = 0.5 * X ** 2 + X + 2 + np.random.randn(m, 1)
+
+    plt.plot(X, y, "b.")
+    plt.xlabel("$x_1$", fontsize=18)
+    plt.ylabel("$y$", rotation=0, fontsize=18)
+    plt.axis([-3, 3, 0, 10])
+    # save_fig("quadratic_data_plot")
+    plt.show()
+
+    from sklearn.preprocessing import PolynomialFeatures
+    poly_features = PolynomialFeatures(degree=2, include_bias=False)
+    X_poly = poly_features.fit_transform(X)
+    print(X[0])
+
+    print(X_poly[0])
+
+    from sklearn.metrics import mean_squared_error
+    from sklearn.model_selection import train_test_split
+
+    def plot_learning_curves(model, X, y):
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=10)
+        train_errors, val_errors = [], []
+        for m in range(1, len(X_train)):
+            model.fit(X_train[:m], y_train[:m])
+            y_train_predict = model.predict(X_train[:m])
+            y_val_predict = model.predict(X_val)
+            train_errors.append(mean_squared_error(y_train[:m], y_train_predict))
+            val_errors.append(mean_squared_error(y_val, y_val_predict))
+
+        plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
+        plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
+        plt.legend(loc="upper right", fontsize=14)  # not shown in the book
+        plt.xlabel("Training set size", fontsize=14)  # not shown
+        plt.ylabel("RMSE", fontsize=14)  # not shown
+
+    lin_reg = LinearRegression()
+    plot_learning_curves(lin_reg, X, y)
+    plt.axis([0, 80, 0, 3])  # not shown in the book
+    # save_fig("underfitting_learning_curves_plot")  # not shown
+    plt.show()  # not shown
+
+    from sklearn.pipeline import Pipeline
+
+    polynomial_regression = Pipeline([
+        ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+        ("lin_reg", LinearRegression()),
+    ])
+
+    plot_learning_curves(polynomial_regression, X, y)
+    plt.axis([0, 80, 0, 3])  # not shown
+    # save_fig("learning_curves_plot")  # not shown
+    plt.show()  # not shown
